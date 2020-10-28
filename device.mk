fb1438f0c395aa2306a0a8365b331eff8e503f88
Gagan Malvi <malvigagan@gmail.com>
2020-10-28 14:16:49 +0530

    mido: Import Paranoid Android overlays
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,9 @@ $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/configs/overlays/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/configs/overlays/overlay \
+    $(LOCAL_PATH)/configs/overlays/overlay-pa
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
