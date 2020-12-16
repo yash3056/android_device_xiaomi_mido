93f5190e40fbdf8921773cc3c70c4e25765a7a0d
Gagan Malvi <malvigagan@gmail.com>
2020-12-16 08:32:17 +0530

    mido: Use common vibrator component
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -366,11 +366,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++.vendor
@@ -408,4 +403,5 @@ PRODUCT_PACKAGES += \
 
 # device/qcom/common modules
 TARGET_COMMON_QTI_COMPONENTS := \
-    usb
+    usb \
+    vibrator
