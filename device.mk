7dad409d7b54f898f866ba6755584fa5f050f935
Gagan Malvi <gagan@malvi.ml>
2020-12-17 15:26:31 +0000

    mido: Revert "Use common vibrator component"
    * Breaks vibration, revert
    
    This reverts commit 4a57ceae982372cb557c7bd41c1457c9b6e07a22.
    
    Change-Id: I520d12c5b8235ffd112a7f79351fce2468396416

--- device.mk
+++ device.mk
@@ -367,6 +367,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++.vendor
@@ -405,5 +410,4 @@ PRODUCT_PACKAGES += \
 # device/qcom/common modules
 TARGET_COMMON_QTI_COMPONENTS := \
     usb \
-    vibrator \
     perf
