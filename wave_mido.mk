8ad0784a95e798b09d39fa6c58930425bb8ecad1
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-22 15:38:53 +0530

    drop common_full_phone.mk

--- wave_mido.mk
+++ wave_mido.mk
@@ -26,9 +26,6 @@ $(call inherit-product, vendor/wave/configs/common.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/wave/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := wave_mido
