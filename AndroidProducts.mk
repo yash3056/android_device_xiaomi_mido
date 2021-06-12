7ab8a81882a98669c64f1b0265c79fc6a62fcc3b
yashlearnpython <yashnarayanp8@gmail.com>
2021-06-12 05:43:29 +0000

    cygnus bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/cygnus_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    cygnus_mido-user \
+    cygnus_mido-userdebug \
+    cygnus_mido-eng
